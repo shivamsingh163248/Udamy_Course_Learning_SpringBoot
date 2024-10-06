66952a5d7bbcb099027acfa81ad05966bd4a26d6
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-06 22:52:20 +0530

    updating the values

--- SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/Application.java
+++ SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/Application.java
@@ -1,16 +1,23 @@
 package Springboot_App.SpringBoot.Fundamental_R;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-	  ApplicationContext applicationContext = (ApplicationContext) SpringApplication.run(Application.class, args);
+	 ApplicationContext applicationContext =  SpringApplication.run(Application.class, args);
 
 	  // creating the arrays and testing all the print
+		// application interface is the
+		String[] bean = applicationContext.getBeanDefinitionNames() ;
+		// creating the for leach loop
+		for(String beans :bean){
+			System.out.println(bean);
+		}
+
 	}
 
 }
