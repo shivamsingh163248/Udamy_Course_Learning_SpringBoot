f43f71188c25b5a4be520e02f655cb25d3e63cf3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-07 00:53:50 +0530

    return in the jason from

--- SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/Contoller/StudentData.java
+++ SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/Contoller/StudentData.java
@@ -4,6 +4,9 @@ import Springboot_App.SpringBoot.Fundamental_R.bean.StudentDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // using the rest controller combination of the two annotation for the handel the api
 @RestController
 public class StudentData {
@@ -14,4 +17,19 @@ public class StudentData {
     public StudentDetails studentDetails(){
         return new StudentDetails("shvam singh " , 3434 , "satyendra kumar singh " , "sarita singh ") ;
     }
+
+    // creating the api for the return data in the form of list in the jason
+    @GetMapping("/allStudent")
+    public List<StudentDetails> AllStudentData(){
+
+        // creating the list and adding the all the data in the list
+        List<StudentDetails>AllStudent = new ArrayList<>() ;
+        // creating the object of the class and the adding in the list
+        AllStudent.add(new StudentDetails("shivam" , 3443 , "satyendra kumar singh " , "sarita singh ")) ;
+        AllStudent.add(new StudentDetails("anuj singh " , 4354675 , "rb singh " , "geeta signh")) ;
+        AllStudent.add(new StudentDetails("lovely singh " , 8369 , "taj singh " , "mata rani")) ;
+        return AllStudent;
+    }
+
+
 }
