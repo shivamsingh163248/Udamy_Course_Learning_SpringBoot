9dde8b01b03a497f655afbe2ff513214303e8c86
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-06 12:23:05 +0530

    learning and updating

---
+++ SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/test/java/Springboot_App/SpringBoot/Fundamental_R/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Springboot_App.SpringBoot.Fundamental_R;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
