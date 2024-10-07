e71f16509c575d263772e6e642fe03166b3592a0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-08 02:20:15 +0530

    learning the path parameter in as the Rest API

---
+++ SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/studentBean/TranslationDetails.java
@@ -0,0 +1,9 @@
+package Springboot_App.SpringBoot.Fundamental_R.studentBean;
+
+public class TranslationDetails {
+
+    // creating the TranslationDetails
+    private int amount;
+    private int date  ;
+
+}
--- SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/studentBean/bankTransacton.java
+++ SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/studentBean/bankTransacton.java
@@ -3,5 +3,17 @@ package Springboot_App.SpringBoot.Fundamental_R.studentBean;
 public class bankTransacton {
 
     // creating the class and check the bank transaction
+    private int accountNumber ;
+    private String name ;
+    private String fatherName ;
 
+    // creating the constructor of the class
+    private static int totalAmount ;
+
+
+    public bankTransacton(int accountNumber, String name, String fatherName) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.fatherName = fatherName;
+    }
 }
