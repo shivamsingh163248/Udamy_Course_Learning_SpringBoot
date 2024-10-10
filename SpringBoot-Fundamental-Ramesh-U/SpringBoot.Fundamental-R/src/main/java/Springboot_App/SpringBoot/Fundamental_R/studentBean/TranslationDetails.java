42da46da2463ec01f715432e5a9a2776f68c2bd2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-11 02:54:40 +0530

    updating the some product

--- SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/studentBean/TranslationDetails.java
+++ SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/studentBean/TranslationDetails.java
@@ -5,5 +5,56 @@ public class TranslationDetails {
     // creating the TranslationDetails
     private int amount;
     private int date  ;
+    private  int debitAmount ;
+    private int CreditAmount;
+    private static int totalAmount ;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getDebitAmount() {
+        return debitAmount;
+    }
+
+    public void setDebitAmount(int debitAmount) {
+        this.debitAmount = debitAmount;
+    }
+
+    public int getCreditAmount() {
+        return CreditAmount;
+    }
+
+    public void setCreditAmount(int creditAmount) {
+        CreditAmount = creditAmount;
+    }
+
+    public static int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public static void setTotalAmount(int totalAmount) {
+        TranslationDetails.totalAmount = totalAmount;
+    }
+
+
+    public TranslationDetails(int amount, int date, int debitAmount, int creditAmount) {
+        this.amount = amount;
+        this.date = date;
+        this.debitAmount = debitAmount;
+        CreditAmount = creditAmount;
+    }
 
 }
--- SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/studentBean/bankTransacton.java
+++ SpringBoot-Fundamental-Ramesh-U/SpringBoot.Fundamental-R/src/main/java/Springboot_App/SpringBoot/Fundamental_R/studentBean/bankTransacton.java
@@ -1,5 +1,7 @@
 package Springboot_App.SpringBoot.Fundamental_R.studentBean;
 
+import java.util.List;
+
 public class bankTransacton {
 
     // creating the class and check the bank transaction
@@ -8,12 +10,39 @@ public class bankTransacton {
     private String fatherName ;
 
     // creating the constructor of the class
-    private static int totalAmount ;
 
+
+    private List<TranslationDetails>translationDetails ;
 
     public bankTransacton(int accountNumber, String name, String fatherName) {
         this.accountNumber = accountNumber;
         this.name = name;
         this.fatherName = fatherName;
     }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+
 }
