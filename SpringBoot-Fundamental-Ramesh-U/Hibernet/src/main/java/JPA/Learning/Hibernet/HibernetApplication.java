cd1cff4c71493dd674396c533fde0e947d5298cf
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-31 12:23:35 +0530

    updating and the learning the jpa

---
+++ SpringBoot-Fundamental-Ramesh-U/Hibernet/src/main/java/JPA/Learning/Hibernet/HibernetApplication.java
@@ -0,0 +1,13 @@
+package JPA.Learning.Hibernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernetApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Fundamental-Ramesh-U/Hibernet/src/test/java/JPA/Learning/Hibernet/HibernetApplicationTests.java
@@ -0,0 +1,13 @@
+package JPA.Learning.Hibernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
