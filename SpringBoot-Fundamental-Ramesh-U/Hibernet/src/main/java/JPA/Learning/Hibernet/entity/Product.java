c697ad92be1d7bd532ea819349d5b7d79fc6d32f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-11-01 02:18:24 +0530

    learning the lambok libraery

--- SpringBoot-Fundamental-Ramesh-U/Hibernet/src/main/java/JPA/Learning/Hibernet/entity/Product.java
+++ SpringBoot-Fundamental-Ramesh-U/Hibernet/src/main/java/JPA/Learning/Hibernet/entity/Product.java
@@ -1,10 +1,20 @@
 package JPA.Learning.Hibernet.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+
+// creating the lambok annotation for the reducing the code
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "ProductLog" , schema = "ecommerse" , uniqueConstraints = {
@@ -37,75 +47,4 @@ public class Product {
     // creating the getter and setter method
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProductName() {
-        return ProductName;
-    }
-
-    public void setProductName(String productName) {
-        ProductName = productName;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return Description;
-    }
-
-    public void setDescription(String description) {
-        Description = description;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public LocalDateTime getDataDateTimeCreated() {
-        return dataDateTimeCreated;
-    }
-
-    public void setDataDateTimeCreated(LocalDateTime dataDateTimeCreated) {
-        this.dataDateTimeCreated = dataDateTimeCreated;
-    }
-
-    public LocalDateTime getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(LocalDateTime lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
 }
