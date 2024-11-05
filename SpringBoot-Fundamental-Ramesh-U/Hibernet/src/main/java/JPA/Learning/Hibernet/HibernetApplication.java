160bba75445b1efa728fbc65514a5a1430fe4f63
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-11-05 23:57:15 +0530

    learning the running the jpa with the two method

--- SpringBoot-Fundamental-Ramesh-U/Hibernet/src/main/java/JPA/Learning/Hibernet/HibernetApplication.java
+++ SpringBoot-Fundamental-Ramesh-U/Hibernet/src/main/java/JPA/Learning/Hibernet/HibernetApplication.java
@@ -1,13 +1,18 @@
 package JPA.Learning.Hibernet;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernetApplication {
+public class HibernetApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernetApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
---
+++ SpringBoot-Fundamental-Ramesh-U/Hibernet/src/test/java/JPA/Learning/Hibernet/Repository/ProductRepositoryTest.java
@@ -0,0 +1,7 @@
+package JPA.Learning.Hibernet.Repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+}
