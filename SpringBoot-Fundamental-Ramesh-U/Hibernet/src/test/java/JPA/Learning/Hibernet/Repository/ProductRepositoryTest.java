a58726d56354986c57d92853c6805ebebb01b084
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-11-06 00:37:20 +0530

    writing in the test to sace the entity in the data bases

--- SpringBoot-Fundamental-Ramesh-U/Hibernet/src/test/java/JPA/Learning/Hibernet/Repository/ProductRepositoryTest.java
+++ SpringBoot-Fundamental-Ramesh-U/Hibernet/src/test/java/JPA/Learning/Hibernet/Repository/ProductRepositoryTest.java
@@ -1,7 +1,18 @@
 package JPA.Learning.Hibernet.Repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@DataJpaTest
 class ProductRepositoryTest {
+
+    // creating the field based dependency injection
+    @Autowired
+    private ProductRepository productRepository ;
+
 
 }
