b16e50f84708a44ea022bf5143096328abe6addf
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-11-16 01:33:17 +0530

    updating

--- SpringBoot-Fundamental-Ramesh-U/Springboot-restful-web-Service/src/main/java/net/shivam/NIT/Springboot_restful_web_Service/Service/Impl/UserServiceImpl.java
+++ SpringBoot-Fundamental-Ramesh-U/Springboot-restful-web-Service/src/main/java/net/shivam/NIT/Springboot_restful_web_Service/Service/Impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package net.shivam.NIT.Springboot_restful_web_Service.Service.Impl;
 
+import lombok.AllArgsConstructor;
 import net.shivam.NIT.Springboot_restful_web_Service.Entity.User;
+import net.shivam.NIT.Springboot_restful_web_Service.Reposistory.UserReposistory;
 import net.shivam.NIT.Springboot_restful_web_Service.Service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
-
+  private UserReposistory userReposistory ;
 
     @Override
     public User CreateUser(User user) {
