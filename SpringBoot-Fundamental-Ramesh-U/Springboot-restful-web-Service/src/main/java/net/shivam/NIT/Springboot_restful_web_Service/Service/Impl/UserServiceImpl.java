5b709f1cfd7d756b169394ab6fc99470c18300df
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-11-16 01:31:02 +0530

    updating the velus

---
+++ SpringBoot-Fundamental-Ramesh-U/Springboot-restful-web-Service/src/main/java/net/shivam/NIT/Springboot_restful_web_Service/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package net.shivam.NIT.Springboot_restful_web_Service.Service.Impl;
+
+import net.shivam.NIT.Springboot_restful_web_Service.Entity.User;
+import net.shivam.NIT.Springboot_restful_web_Service.Service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+
+
+    @Override
+    public User CreateUser(User user) {
+        return null;
+    }
+}
---
+++ SpringBoot-Fundamental-Ramesh-U/Springboot-restful-web-Service/src/main/java/net/shivam/NIT/Springboot_restful_web_Service/Service/UserService.java
@@ -0,0 +1,9 @@
+package net.shivam.NIT.Springboot_restful_web_Service.Service;
+
+import net.shivam.NIT.Springboot_restful_web_Service.Entity.User;
+
+public interface UserService {
+
+    // creating the method userRepository
+    User CreateUser(User user) ;
+}
