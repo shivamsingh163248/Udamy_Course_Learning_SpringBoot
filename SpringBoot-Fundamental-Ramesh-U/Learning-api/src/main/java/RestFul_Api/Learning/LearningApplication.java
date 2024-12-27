10e396d9bf43abdc68cfba8cf8368d9508969c0a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-12-27 20:26:48 +0530

    updating new project

---
+++ SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/LearningApplication.java
@@ -0,0 +1,13 @@
+package RestFul_Api.Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningApplication.class, args);
+	}
+
+}
