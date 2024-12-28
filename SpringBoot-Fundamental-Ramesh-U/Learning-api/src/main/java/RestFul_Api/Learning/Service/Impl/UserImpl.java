f974dc99fd12e6ec592a76fd27040a3d43d670ce
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-12-28 15:59:59 +0530

    updating

--- SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/Service/Impl/UserImpl.java
+++ SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/Service/Impl/UserImpl.java
@@ -1,4 +1,6 @@
 package RestFul_Api.Learning.Service.Impl;
 
 public class UserImpl {
+
+    // now implement all the userinterface
 }
--- SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/Service/UserInterface.java
+++ SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/Service/UserInterface.java
@@ -1,7 +1,10 @@
 package RestFul_Api.Learning.Service;
 
+import RestFul_Api.Learning.Enity.User;
+
 public interface UserInterface {
 
     // now creating the user
+    public User CreateUser(User user) ;
 
 }
