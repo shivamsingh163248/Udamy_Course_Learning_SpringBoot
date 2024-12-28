b4fb1bd88ebdaecf5fe07763e3f4fe8c73558709
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-12-28 08:17:06 +0530

    updating

--- SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/Enity/User.java
+++ SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/Enity/User.java
@@ -1,17 +1,29 @@
 package RestFul_Api.Learning.Enity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User {
 
     // creating the fields for the user details
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
+    @Column(nullable = false)
     private String name ;
+    @Column(nullable = false)
     private String last_name ;
+    @Column(nullable = false , unique = true)
     private int RollNo ;
+    @Column(nullable = false , unique = true)
     private int email ;
 
 }
