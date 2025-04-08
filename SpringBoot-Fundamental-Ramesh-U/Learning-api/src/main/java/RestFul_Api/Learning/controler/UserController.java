2ff4d080f2766b2af982b4ad136e9d0f0da5f945
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-04-09 00:30:17 +0530

    update

--- SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/controler/UserController.java
+++ SpringBoot-Fundamental-Ramesh-U/Learning-api/src/main/java/RestFul_Api/Learning/controler/UserController.java
@@ -1,4 +1,15 @@
 package RestFul_Api.Learning.controler;
 
+import RestFul_Api.Learning.Enity.User;
+import org.springframework.http.ResponseEntity;
+
 public class UserController {
+
+    // creating the function takes a input user
+    public ResponseEntity<User> createUser(User user){
+
+        // hear is the return
+
+    }
+
 }
